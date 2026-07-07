323be02f53a1f27c2c00d30ec9a3f8879a836005
nnnsss12 <ms.nora1424@gmail.com>
2026-07-07 15:01:53 +0200

    Fix typos in app init and run files

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 from flask_restx import Api
 
 def create_app():
-    app = Flask(name)
+    app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
     return app
--- part2/run.py
+++ part2/run.py
@@ -2,5 +2,5 @@ from app import create_app
 
 app = create_app()
 
-if name == 'main':
+if __name__ == '__main__':
     app.run(debug=True)
