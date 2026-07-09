5c06bded6a0a09be9810e55f93e9266e408d2fde
Lama <lamamqqz@gmail.com>
2026-07-09 16:44:37 +0300

    add: base model implementation

---
+++ part2/app/models/base_model.py
@@ -0,0 +1,31 @@
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        """Update the updated_at timestamp whenever the object is modified"""
+        self.updated_at = datetime.now()
+
+    def update(self, data):
+        """Update the attributes of the object based on the provided dictionary"""
+        for key, value in data.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+        self.save()  # Update the updated_at timestamp
+        
+    def create(self):
+        """Create/register a new instance
+        (calls save to set initial timestamps)
+        """
+        self.save()
+
+    def delete(self):
+        """Delete the current instance
+        (placeholder for persistence layer removal)
+        """
+        del self
