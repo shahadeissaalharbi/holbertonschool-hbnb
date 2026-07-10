3dae5f2a21dbb7ecf0f8c47c30ff4b9a6cfa19fd
Lama <lamamqqz@gmail.com>
2026-07-10 10:38:31 +0300

    add: implementation for classes user, place, and place_amenity class

---
+++ part2/app/models/place.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+"""Defines the Place class"""
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+    """Represents a place that can be listed and reviewed"""
+
+    def __init__(self, title, description, price,
+                 latitude, longitude, owner):
+        """Initialize a new Place instance"""
+        super().__init__()
+        self.title = title
+        self.description = description
+        self.price = price
+        self.latitude = latitude
+        self.longitude = longitude
+        self.owner = owner
+        self.reviews = []
+        self.amenities = []
+
+    @property
+    def title(self):
+        """Get the title"""
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        """Validate and set the title"""
+        if not value or len(value) > 100:
+            raise ValueError(
+                "title is required, max 100 characters")
+        self._title = value
+
+    @property
+    def price(self):
+        """Get the price"""
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        """Validate and set the price"""
+        if value <= 0:
+            raise ValueError("price must be a positive value")
+        self._price = float(value)
+
+    @property
+    def latitude(self):
+        """Get the latitude"""
+        return self._latitude
+
+    @latitude.setter
+    def latitude(self, value):
+        """Validate and set the latitude"""
+        if not (-90.0 <= value <= 90.0):
+            raise ValueError("latitude must be between -90 and 90")
+        self._latitude = float(value)
+
+    @property
+    def longitude(self):
+        """Get the longitude"""
+        return self._longitude
+
+    @longitude.setter
+    def longitude(self, value):
+        """Validate and set the longitude"""
+        if not (-180.0 <= value <= 180.0):
+            raise ValueError("longitude must be between -180 and 180")
+        self._longitude = float(value)
+
+    @property
+    def owner(self):
+        """Get the owner"""
+        return self._owner
+
+    @owner.setter
+    def owner(self, value):
+        """Validate and set the owner"""
+        if not isinstance(value, User):
+            raise ValueError("owner must be a valid User instance")
+        self._owner = value
+
+    def add_review(self, review):
+        """Add a review to the place"""
+        self.reviews.append(review)
+
+    def add_amenity(self, amenity):
+        """Add an amenity to the place"""
+        self.amenities.append(amenity)
---
+++ part2/app/models/user.py
@@ -0,0 +1,89 @@
+import re
+from app.models.base_model import BaseModel
+class User(BaseModel):
+    def __init__(self, first_name, last_name, email, password, is_admin=False):
+        super().__init__()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.is_admin = is_admin
+        
+    @property
+    def first_name(self):
+        """get the first name
+        """
+        return self._first_name
+    
+    @first_name.setter
+    def first_name(self, value):
+        """Validate and set the first name
+        """
+        if not value or not isinstance(value, str) or value > 50:
+            raise ValueError("First name must be string and under 50 characters")
+        self._first_name = value
+      
+    
+    @property
+    def last_name(self):
+        """get the last name
+        """
+        return self._last_name
+    
+    @last_name.setter
+    def last_name(self, value):
+        """Validate and set the last name
+        """
+        if not value or not isinstance(value, str) or value > 50:
+            raise ValueError("Last name must be string and under 50 characters")
+        self._last_name = value
+        
+    @property
+    def email(self):
+        """get email
+        """
+        return self.email
+    
+    @email.setter
+    def email(self, value):
+        """Validate and set the email
+        """
+        pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
+        if not value or not re.match(pattern, value):
+            raise ValueError("email must be a valid email address")
+        self._email = value
+        
+    @property
+    def password(self):
+        """get the password"""
+        return self._password
+
+    @password.setter
+    def password(self, value):
+        """Validate and set the password"""
+        if not value or len(value) < 6:
+            raise ValueError(
+                "password is required, minimum 6 characters")
+        self._password = value
+    
+    @property
+    def is_admin(self):
+        """Get the admin status"""
+        return self._is_admin
+
+    @is_admin.setter
+    def is_admin(self, value):
+        """Validate and set the admin status"""
+        if not isinstance(value, bool):
+            raise ValueError("is_admin must be a boolean")
+        self._is_admin = value
+    
+    
+       
+       
+        
+
+        
+     
+        
+    
