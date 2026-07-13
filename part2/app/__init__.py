c7f1fad7a456e37d1a9d8432fe7850ebbbd6771f
shahad <shahadeissa111@gmail.com>
2026-07-13 17:50:00 +0300

    add amenity endpoint

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from flask_restx import Api
+from part2.app import api
 
 def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
+    return app
+
+    from app.api.v1.amenities import api as amenities_ns
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
     return app
