71586774fca3dbee356190d78a7cba4ee65aeed3
Lama <lamamqqz@gmail.com>
2026-07-14 16:42:58 +0300

    add: try catch in registering new user

--- part2/app/api/v1/users.py
+++ part2/app/api/v1/users.py
@@ -18,17 +18,21 @@ class UserList(Resource):
     @api.response(400, 'Invalid input data')
     def post(self):
         """Register a new user"""
+    
         user_data = api.payload
-
         existing_user = facade.get_user_by_email(user_data['email'])
         if existing_user:
             return {'error': 'Email already registered'}, 400
 
-        new_user = facade.create_user(user_data)
+        try:
+            new_user = facade.create_user(user_data)
+        except ValueError as e:
+            return {'error': str(e)}, 400
+
         return {
-            'id': new_user.id, 
-            'first_name': new_user.first_name, 
-            'last_name': new_user.last_name, 
+            'id': new_user.id,
+            'first_name': new_user.first_name,
+            'last_name': new_user.last_name,
             'email': new_user.email
         }, 201
 
