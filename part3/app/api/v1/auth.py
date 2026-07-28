288e45bc7c1658cbd37131f9bcac6da3ce97af14
nnnsss12 <ms.nora1424@gmail.com>
2026-07-28 15:15:32 +0300

    add protected api

--- part3/app/api/v1/auth.py
+++ part3/app/api/v1/auth.py
@@ -1,7 +1,7 @@
 from flask_restx import Namespace, Resource, fields
 from flask_jwt_extended import create_access_token
 from app.services import facade
-from flask_jwt_extended import jwt_required, get_jwt_identity
+
 
 api = Namespace('auth', description='Authentication operations')
 
@@ -35,15 +35,3 @@ class Login(Resource):
         return {'access_token': access_token}, 200
     
 
-@api.route('/protected')
-class ProtectedResource(Resource):
-    @jwt_required()
-    def get(self):
-         """A protected endpoint that requires a valid JWT token"""
-         print("jwt------")
-         print(get_jwt_identity())
-         current_user = get_jwt_identity() # Retrieve the user's identity from the token
-         #if you need to see if the user is an admin or not, you can access additional claims using get_jwt() :
-         # addtional claims = get_jwt()
-         #additional claims["is_admin"] -> True or False
-         return {'message': f'Hello, user {current_user}'}, 200
